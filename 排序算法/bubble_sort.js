ceab18125aa6eb18bbdad73fce9a2415398b16bf
秋秋 <2684196398@qq.com>
2022-03-07 16:55:38 +0800

    冒泡排序

--- 排序算法/bubble_sort.js
+++ 排序算法/bubble_sort.js
@@ -3,13 +3,12 @@ function bubbleSort(arr) {
   
   for (let i = 0; i < arr.length - 1; i++) {
     let swapFlag = false;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
         swapFlag = true;
-        console.log("交换")
       }
     }
     if (!swapFlag) break;
